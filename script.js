9616944d9754bffb0672147cc6ffd92c68d2dc0a
BigguyFM98000 <fm.raphalalani29@gmail.com>
2024-10-28 11:17:20 +0200

    added images to sections

---
+++ script.js
@@ -0,0 +1,11 @@
+// Smooth scrolling function
+function smoothScroll(target) {
+    document.querySelector(target).scrollIntoView({
+        behavior: 'smooth'
+    });
+}
+
+// Alert function for donation buttons
+function donateAlert() {
+    alert("Thank you for your generosity!");
+}
